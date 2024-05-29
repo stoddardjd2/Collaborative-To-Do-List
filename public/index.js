1ba5a023e5c5495ec0bd5de04b7788e02f21c37e
Jared Stoddard <jared.stoddard2@gmail.com>
2024-05-28 20:22:05 -0700

    added button and functionality for making new notes

--- public/index.js
+++ public/index.js
@@ -1,45 +1,63 @@
-console.log("javascript running!");
 
 let notes = [];
 
 window.onload = function () {
-    var btn = document.getElementById('btn');
-    btn.addEventListener("click", function confirm() {
-        var list = document.getElementById("notes").getElementsByTagName("li");
-        notes = [];
-        //clear notes before adding current notes to object
-        for(i=0;i<=list.length-1;i++){
-            var text = list[i].getElementsByTagName("textarea")[0].value;
-            
-            console.log("text:");
-            console.log(text);
-            notes.push({
-              id:i,
-              noteText: text
-            })
-        }
-        //save innerText of each li to an object to be sent as a post request
+  var newNoteBtn = document.getElementById("newNote");
+  var saveBtn = document.getElementById('save');
 
-        console.log(notes)
+  newNoteBtn.addEventListener("click", function () {
+    var ul = document.getElementById("notes");
+    var li = document.createElement('li');
+    var textarea = document.createElement('textarea');
+    textarea.innerHTML="new note!";
+    li.appendChild(textarea);
+    ul.appendChild(li);
+    //create new li and text area elements and add them to the ul to be used as a new note
+  })
+  //check if add new note button is clicked and adds note if so
 
-        saveReq();
-        //get text value of each li to be sent to server and saved in database
+  saveBtn.addEventListener("click", function () {
+    var list = document.getElementById("notes").getElementsByTagName("li");
+    notes = [];
+    //clear notes before adding current notes to object
+    for (i = 0; i <= list.length - 1; i++) {
+      var text = list[i].getElementsByTagName("textarea")[0].value;
 
-        //add feature to detect which notes were changed and save only what is needed
-        
-    });
+      console.log("text:");
+      console.log(text);
+      notes.push({
+        id: i,
+        noteText: text
+      })
+    }
+    //save innerText of each li to an object to be sent as a post request
+
+    console.log(notes)
+
+    saveReq();
+    //get text value of each li to be sent to server and saved in database
+
+    //add feature to detect which notes were changed and save only what is needed
+
+
+
+  });
 }
 
 
-function saveReq(){
-    fetch('/save', {
-        method: "POST",
-        body: JSON.stringify({
-          notes
-        }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          }
-      })
+
+
+
+
+function saveReq() {
+  fetch('/save', {
+    method: "POST",
+    body: JSON.stringify({
+      notes
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
 }
 
